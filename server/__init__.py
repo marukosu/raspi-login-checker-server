6b214b60e45120fc90671a53b3529cad2475d4fa
utsushiiro <ozeki.haru@gmail.com>
2018-04-10 21:12:05 +0900

    serverモジュールのinitにデータベース接続処理を追加

--- server/__init__.py
+++ server/__init__.py
@@ -1,5 +1,9 @@
 from flask import Flask
 from .controllers import app as blueprint
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.register_blueprint(blueprint)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:@localhost/raspi-login-checker?charset=utf8'
+db = SQLAlchemy(app)
