631c0f94c6d23fb7b8907ad32c27c35e1a91d150
utsushiiro <ozeki.haru@gmail.com>
2018-04-11 00:15:01 +0900

    Userの__init__の余分な引数を削除

--- server/models/user.py
+++ server/models/user.py
@@ -6,9 +6,8 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), index=True, nullable=False, unique=True)
 
-    def __init__(self, username, email):
+    def __init__(self, username):
         self.username = username
-        self.email = email
 
     def __repr__(self):
         return '<User {}>'.format(self.username)
