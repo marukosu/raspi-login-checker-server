1d87930eb50c72ca88474d7ca900695178fd1d12
utsushiiro <ozeki.haru@gmail.com>
2018-04-12 22:36:09 +0900

    各種設定を管理するconfigディレクトリを作成

---
+++ config/development.py
@@ -0,0 +1,2 @@
+DEBUG = True
+SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:@localhost/raspi_login_checker?charset=utf8'
--- server/__init__.py
+++ server/__init__.py
@@ -3,9 +3,12 @@ from .controllers import app as blueprint
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+
+app.config.from_pyfile('../config/default.py')
+app.config.from_envvar('FLASK_CONFIG_FILE', silent=True)
+
 app.register_blueprint(blueprint)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:@localhost/raspi_login_checker?charset=utf8'
 db = SQLAlchemy(app)
 
 from .models import *
