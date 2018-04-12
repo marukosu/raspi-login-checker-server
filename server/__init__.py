a6c0aad95dfb3accc790c0d33c48146a5e26baad
utsushiiro <ozeki.haru@gmail.com>
2018-04-12 23:41:02 +0900

    serverモジュールの__init__での初期化処理の順番を修正

--- server/__init__.py
+++ server/__init__.py
@@ -1,14 +1,13 @@
 from flask import Flask
-from .controllers import app as blueprint
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-
 app.config.from_pyfile('../config/default.py')
-app.config.from_envvar('FLASK_CONFIG_FILE', silent=True)
-
-app.register_blueprint(blueprint)
-
+app.config.from_envvar('FLASK_CONFIG_FILE')
 db = SQLAlchemy(app)
 
 from .models import *
+
+from .controllers import app as blueprint
+
+app.register_blueprint(blueprint)
