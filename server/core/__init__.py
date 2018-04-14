01c2eb561657c8109b58e14debf212c1da62f781
utsushiiro <ozeki.haru@gmail.com>
2018-04-14 21:11:47 +0900

    configのパスを修正

--- server/core/__init__.py
+++ server/core/__init__.py
@@ -2,6 +2,6 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config.from_pyfile('../config/default.py')
+app.config.from_pyfile('../../config/default.py')
 app.config.from_envvar('FLASK_CONFIG_FILE')
 db = SQLAlchemy(app)
